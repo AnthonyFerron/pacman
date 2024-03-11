07c0d6dfe182aa6ff7d6ece8d52215320756b16f
Lasergtx <131684618+Lasergtx@users.noreply.github.com>
2024-03-11 16:37:49 +0100

    aa

--- main.c
+++ main.c
@@ -1,7 +1,23 @@
 #include <SDL2/SDL.h>
+#include "pacman.h"
 
 int main(int argc, char *argv[])
 {
-    SDL_Init(SDL_INIT_VIDEO);
-    return 0;
+    SDL_Window *window = NULL;
+    if(0 != SDL_Init(SDL_INIT_VIDEO))
+    {
+        fprintf(stderr, "Erreur SDL_Init : %s", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+    window = SDL_CreateWindow("SDL2", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                              640, 480, SDL_WINDOW_SHOWN);
+    if(NULL == window)
+    {
+        fprintf(stderr, "Erreur SDL_CreateWindow : %s", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+    /* On agit sur la fenêtre ici */
+    SDL_Delay(3000);
+    SDL_Quit();
+    return EXIT_SUCCESS;
 }
