b7803c025229a28e2e57cc4b4cf59926284b2746
AnthonyFerron <anthony.ferron74@gmail.com>
2024-03-13 15:14:02 +0100

    aa

---
+++ pacman.h
@@ -0,0 +1,15 @@
+#ifndef PACMAN
+#define PACMAN
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL2/SDL.h>
+
+#define WINDOW_WIDTH 1250
+#define WINDOW_HEIGHT 750
+
+int theMain();
+#include "game.c"
+
+
+#endif
