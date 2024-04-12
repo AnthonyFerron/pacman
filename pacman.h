423d3ea18cb16dfc650a23ba8e52579cd283dd5a
AnthonyFerron <anthony.ferron74@gmail.com>
2024-04-12 12:02:05 +0200

    colisions + maps ok

--- pacman.h
+++ pacman.h
@@ -7,11 +7,11 @@
 #include <SDL.h>
 #include <stdbool.h>
 
-#define WINDOW_WIDTH 1250
-#define WINDOW_HEIGHT 750
+#define WINDOW_WIDTH 1350
+#define WINDOW_HEIGHT 850
 #define TILE_SIZE 50
-#define MAP_WIDTH 25
-#define MAP_HEIGHT 15
+#define MAP_WIDTH 27
+#define MAP_HEIGHT 17
 
 int theMain();
 #include "game.c"
